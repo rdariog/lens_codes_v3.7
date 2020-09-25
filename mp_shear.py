27e6c3559dc07e8c9efdf2cda3dfabc3f6183a73
Dario g <dariog@famaf.unc.edu.ar>
2020-09-25 16:25:48 -0300

    cleaning some code stuffs

--- mp_shear.py
+++ mp_shear.py
@@ -1,10 +1,15 @@
+"""
+New multipole share parallel calculation
+"""
+
 import numpy as np
 
 from astropy.cosmology import LambdaCDM
-from scipy.misc import derivative
-from scipy import integrate
+# from scipy.misc import derivative
+# from scipy import integrate
+import profiles_fit
 
-class mp_shear():
+class mp_shear:
     """
     Equations from van Uitert (vU, arXiv:1610.04226) for the
     multipole expansion and from Ford et al. (F,2015) for
@@ -54,131 +59,149 @@ class mp_shear():
                     sin(2theta) (vU_Eq17) for considering kappa_offset
     """
 
-    cvel = 299792458;   # Speed of light (m.s-1)
-    G    = 6.670e-11;   # Gravitational constant (m3.kg-1.s-2)
-    pc   = 3.085678e16; # 1 pc (m)
+    cvel = 299792458    # Speed of light (m.s-1)
+    G    = 6.670e-11    # Gravitational constant (m3.kg-1.s-2)
+    pc   = 3.085678e16  # 1 pc (m)
     Msun = 1.989e30     # Solar mass (kg)
 
     def __init__(self, r, M200=1.e14, ellip=0.25, z=0.2, h=0.7,
                         misscentred=False, s_off=0.4,
                         components = ['t0', 't', 'tcos', 'xsin'],
                         verbose=True, Yanmiss = False):
+        """
+        init method, it sets up all variables
+        """
 
-        self.r           = review
-        self.M200        = 1.e14
-        self.ellip       = 0.25
-        self.z           = 0.2
-        self.h           = 0.7
-        self.misscentred = False
-        self.s_off       = 0.4
-        self.components  = ['t0', 't', 'tcos', 'xsin']
-        self.verbose     = True
-        Yanmiss          = False
+        if not isinstance(r, (np.ndarray)):
+            r = np.array([r])
 
-    def Delta_Sigma(R):
-        '''
-        Density contraste for NFW
+        self.r           = r
+        self.M200        = M200
+        self.ellip       = ellip
+        self.z           = z
+        self.h           = h
+        self.misscentred = misscentred
+        self.s_off       = s_off
+        self.components  = components
+        self.verbose     = verbose
+        self.Yanmiss     = Yanmiss
 
-        '''
+        # Compute cosmological parameters
+        self.cosmo    = LambdaCDM(H0=self.h*100, Om0=0.3, Ode0=0.7)
+        self.H        = self.cosmo.H(self.z).value / (1.0e3 * self.pc) #H at z_pair s-1
+        self.roc      = (3.0 * (self.H**2.0)) / (8.0*np.pi * self.G) #critical density at z_pair (kg.m-3)
+        self.roc_mpc  = self.roc * ((self.pc * 1.0e6)**3.0)
 
-        #calculo de c usando la relacion de Duffy et al 2008
+        # Compute R_200
+        self.R200 = profiles_fit.r200_nfw(self.M200, self.roc_mpc)
 
-        M=((800.0*np.pi*roc_mpc*(R200**3))/(3.0*Msun))*h
-        c=5.71*((M/2.e12)**-0.084)*((1.+z)**-0.47)
+        # Scaling sigma_off
+        self.s_off = self.s_off / self.h
 
-        ####################################################
-
-        deltac=(200./3.)*( (c**3) / ( np.log(1.+c)- (c/(1+c)) ))
-        x=np.round((R*c)/R200,12)
-        m1= x< 1.0
-        m2= x> 1.0
-        m3= (x == 1.0)
-
-        try:
-            jota=np.zeros(len(x))
-            atanh=np.arctanh(((1.0-x[m1])/(1.0+x[m1]))**0.5)
-            jota[m1]=(4.0*atanh)/((x[m1]**2.0)*((1.0-x[m1]**2.0)**0.5)) \
-                + (2.0*np.log(x[m1]/2.0))/(x[m1]**2.0) - 1.0/(x[m1]**2.0-1.0) \
-                + (2.0*atanh)/((x[m1]**2.0-1.0)*((1.0-x[m1]**2.0)**0.5))
-            atan=np.arctan(((x[m2]-1.0)/(1.0+x[m2]))**0.5)
-            jota[m2]=(4.0*atan)/((x[m2]**2.0)*((x[m2]**2.0-1.0)**0.5)) \
-                + (2.0*np.log(x[m2]/2.0))/(x[m2]**2.0) - 1.0/(x[m2]**2.0-1.0) \
-                + (2.0*atan)/((x[m2]**2.0-1.0)**1.5)
-            jota[m3]=2.0*np.log(0.5)+5.0/3.0
-        except:
-            if m1:
-                atanh=np.arctanh(((1.0-x[m1])/(1.0+x[m1]))**0.5)
-                jota = ((4.0*atanh)/((x[m1]**2.0)*((1.0-x[m1]**2.0)**0.5))
-                    + (2.0*np.log(x[m1]/2.0))/(x[m1]**2.0) - 1.0/(x[m1]**2.0-1.0)
-                    + (2.0*atanh)/((x[m1]**2.0-1.0)*((1.0-x[m1]**2.0)**0.5)))
-            if m2:
-                atan=np.arctan(((x[m2]-1.0)/(1.0+x[m2]))**0.5)
-                jota = ((4.0*atan)/((x[m2]**2.0)*((x[m2]**2.0-1.0)**0.5))
-                    + (2.0*np.log(x[m2]/2.0))/(x[m2]**2.0) - 1.0/(x[m2]**2.0-1.0)
-                    + (2.0*atan)/((x[m2]**2.0-1.0)**1.5))
-            if m3:
-                jota = 2.0*np.log(0.5)+5.0/3.0
-
-
-
-        rs_m=(R200*1.e6*pc)/c
-        kapak=((2.*rs_m*deltac*roc_mpc)*(pc**2/Msun))/((pc*1.0e6)**3.0)
-        return kapak*jota
-
-
-    def monopole(R):
-        '''
-        Projected density for NFW
-
-        '''
-        if not isinstance(R, (np.ndarray)):
-        	R = np.array([R])
-
-        # m = R == 0.
-        # R[m] = 1.e-8
-
-        #calculo de c usando la relacion de Duffy et al 2008
-
-        M=((800.0*np.pi*roc_mpc*(R200**3))/(3.0*Msun))*h
-        c=5.71*((M/2.e12)**-0.084)*((1.+z)**-0.47)
-
-        ####################################################
-
-        deltac=(200./3.)*( (c**3) / ( np.log(1.+c)- (c/(1+c)) ))
-
-        x=(R*c)/R200
-        m1 = x <= (1.0-1.e-12)
-        m2 = x >= (1.0+1.e-12)
-        m3 = (x == 1.0)
-        m4 = (~m1)*(~m2)*(~m3)
-
-        jota  = np.zeros(len(x))
-        atanh = np.arctanh(np.sqrt((1.0-x[m1])/(1.0+x[m1])))
-        jota[m1] = (1./(x[m1]**2-1.))*(1.-(2./np.sqrt(1.-x[m1]**2))*atanh)
-
-        atan = np.arctan(((x[m2]-1.0)/(1.0+x[m2]))**0.5)
-        jota[m2] = (1./(x[m2]**2-1.))*(1.-(2./np.sqrt(x[m2]**2 - 1.))*atan)
-
-        jota[m3] = 1./3.
-
-        x1 = 1.-1.e-4
-        atanh1 = np.arctanh(np.sqrt((1.0-x1)/(1.0+x1)))
-        j1 = (1./(x1**2-1.))*(1.-(2./np.sqrt(1.-x1**2))*atanh1)
-
-        x2 = 1.+1.e-4
-        atan2 = np.arctan(((x2-1.0)/(1.0+x2))**0.5)
-        j2 = (1./(x2**2-1.))*(1.-(2./np.sqrt(x2**2 - 1.))*atan2)
-
-        jota[m4] = np.interp(x[m4].astype(float64),[x1,x2],[j1,j2])
-
-        rs_m=(R200*1.e6*pc)/c
-        kapak=((2.*rs_m*deltac*roc_mpc)*(pc**2/Msun))/((pc*1.0e6)**3.0)
-        return kapak*jota
-
-    def quadrupole(R):
-        '''
-        Quadrupole term defined as (d(Sigma)/dr)*r
-
-        '''
-        m0p = derivative(monopole,R,dx=1e-5)
-        return m0p*R
+    #
+    # def Delta_Sigma(R):
+    #     '''
+    #     Density contraste for NFW
+    #     '''
+    #
+    #     #calculo de c usando la relacion de Duffy et al 2008
+    #
+    #     M = ((800.0 * np.pi * roc_mpc * (R200**3)) / (3.0 * self.Msun)) * self.h
+    #     c = 5.71*((M/2.e12)**-0.084)*((1.+z)**-0.47)
+    #
+    #     ####################################################
+    #
+    #     deltac = (200.0 / 3.0) * ( (c**3.0) / ( np.log(1.0 + c) - (c / (1.0 + c))))
+    #     x = np.round((R * c) / R200, 12)
+    #     m1 = x < 1.0
+    #     m2 = x > 1.0
+    #     m3 = x == 1.0
+    #
+    #     try:
+    #         jota=np.zeros(len(x))
+    #         atanh=np.arctanh(((1.0-x[m1])/(1.0+x[m1]))**0.5)
+    #         jota[m1]=(4.0*atanh)/((x[m1]**2.0)*((1.0-x[m1]**2.0)**0.5)) \
+    #             + (2.0*np.log(x[m1]/2.0))/(x[m1]**2.0) - 1.0/(x[m1]**2.0-1.0) \
+    #             + (2.0*atanh)/((x[m1]**2.0-1.0)*((1.0-x[m1]**2.0)**0.5))
+    #         atan=np.arctan(((x[m2]-1.0)/(1.0+x[m2]))**0.5)
+    #         jota[m2]=(4.0*atan)/((x[m2]**2.0)*((x[m2]**2.0-1.0)**0.5)) \
+    #             + (2.0*np.log(x[m2]/2.0))/(x[m2]**2.0) - 1.0/(x[m2]**2.0-1.0) \
+    #             + (2.0*atan)/((x[m2]**2.0-1.0)**1.5)
+    #         jota[m3]=2.0*np.log(0.5)+5.0/3.0
+    #     except:
+    #         if m1:
+    #             atanh=np.arctanh(((1.0-x[m1])/(1.0+x[m1]))**0.5)
+    #             jota = ((4.0*atanh)/((x[m1]**2.0)*((1.0-x[m1]**2.0)**0.5))
+    #                 + (2.0*np.log(x[m1]/2.0))/(x[m1]**2.0) - 1.0/(x[m1]**2.0-1.0)
+    #                 + (2.0*atanh)/((x[m1]**2.0-1.0)*((1.0-x[m1]**2.0)**0.5)))
+    #         if m2:
+    #             atan=np.arctan(((x[m2]-1.0)/(1.0+x[m2]))**0.5)
+    #             jota = ((4.0*atan)/((x[m2]**2.0)*((x[m2]**2.0-1.0)**0.5))
+    #                 + (2.0*np.log(x[m2]/2.0))/(x[m2]**2.0) - 1.0/(x[m2]**2.0-1.0)
+    #                 + (2.0*atan)/((x[m2]**2.0-1.0)**1.5))
+    #         if m3:
+    #             jota = 2.0*np.log(0.5)+5.0/3.0
+    #
+    #
+    #
+    #     rs_m=(R200*1.e6*self.pc)/c
+    #     kapak=((2.*rs_m*deltac*roc_mpc)*(self.pc**2/Msun))/((self.pc*1.0e6)**3.0)
+    #     return kapak*jota
+    #
+    #
+    # def monopole(R):
+    #     '''
+    #     Projected density for NFW
+    #
+    #     '''
+    #     if not isinstance(R, (np.ndarray)):
+    #         R = np.array([R])
+    #
+    #     # m = R == 0.
+    #     # R[m] = 1.e-8
+    #
+    #     #calculo de c usando la relacion de Duffy et al 2008
+    #
+    #     M=((800.0*np.pi*roc_mpc*(R200**3))/(3.0*Msun))*h
+    #     c=5.71*((M/2.e12)**-0.084)*((1.+z)**-0.47)
+    #
+    #     ####################################################
+    #
+    #     deltac=(200./3.)*( (c**3) / ( np.log(1.+c)- (c/(1+c)) ))
+    #
+    #     x=(R*c)/R200
+    #     m1 = x <= (1.0-1.e-12)
+    #     m2 = x >= (1.0+1.e-12)
+    #     m3 = (x == 1.0)
+    #     m4 = (~m1)*(~m2)*(~m3)
+    #
+    #     jota  = np.zeros(len(x))
+    #     atanh = np.arctanh(np.sqrt((1.0-x[m1])/(1.0+x[m1])))
+    #     jota[m1] = (1./(x[m1]**2-1.))*(1.-(2./np.sqrt(1.-x[m1]**2))*atanh)
+    #
+    #     atan = np.arctan(((x[m2]-1.0)/(1.0+x[m2]))**0.5)
+    #     jota[m2] = (1./(x[m2]**2-1.))*(1.-(2./np.sqrt(x[m2]**2 - 1.))*atan)
+    #
+    #     jota[m3] = 1./3.
+    #
+    #     x1 = 1.-1.e-4
+    #     atanh1 = np.arctanh(np.sqrt((1.0-x1)/(1.0+x1)))
+    #     j1 = (1./(x1**2-1.))*(1.-(2./np.sqrt(1.-x1**2))*atanh1)
+    #
+    #     x2 = 1.+1.e-4
+    #     atan2 = np.arctan(((x2-1.0)/(1.0+x2))**0.5)
+    #     j2 = (1./(x2**2-1.))*(1.-(2./np.sqrt(x2**2 - 1.))*atan2)
+    #
+    #     jota[m4] = np.interp(x[m4].astype(float64),[x1,x2],[j1,j2])
+    #
+    #     rs_m=(R200*1.e6*self.pc)/c
+    #     kapak=((2.*rs_m*deltac*roc_mpc)*(self.pc**2/Msun))/((self.pc*1.0e6)**3.0)
+    #     return kapak*jota
+    #
+    # def quadrupole(R):
+    #     '''
+    #     Quadrupole term defined as (d(Sigma)/dr)*r
+    #
+    #     '''
+    #     m0p = derivative(monopole,R,dx=1e-5)
+    #     return m0p*R
